61c1f2a1aac606ac812c2e3031382f82383b4b54
vanha777 <vanha777@icloud.com>
2022-06-25 09:37:34 +1000

    Update Greeter.sol

--- my-project/hardhat-erc20/contracts/Greeter.sol
+++ my-project/hardhat-erc20/contracts/Greeter.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.4;
 contract ERC20 {
     string public name ;
     string public symbol ;
-    uint256 public supply ;
+    uint256 public supply;
     
     mapping(address=>uint256) public balances ;
+    mapping(address=>mapping(address=>uint256)) public allowances;
 
-    constructor(string memory _name, string memory _symbol) {
+    constructor(string memory _name, string memory _symbol , uint256 _supply) {
         _name = name ;
         _symbol = symbol ;
+        _supply = supply ;
     }
 
     function decimals() external pure returns (uint8) {
@@ -24,10 +26,26 @@ contract ERC20 {
         return true ;
     }
 
-    function transfer(address from, address to, uint256 amount) external returns (bool) {
+    function transfer(address _to, uint256 value) external returns(bool) {
+        return transfertemplate(msg.sender,_to,value);
+    }
+
+    function approve(address receiver, uint256 _Amount ) external returns (bool) {
+        require(balances[msg.sender] >= _Amount , "Insufficienct funds") ;
+        allowances[msg.sender][receiver] = _Amount ;
+        return true;
+    }
+
+    function transferfrom(address _sender , address _receiver , uint256 _amount) external returns (bool) {
+        require(allowances[_sender][msg.sender] >= _amount , "Allowances Limit Reached") ;
+        uint256 currentallowances = allowances[_sender][_receiver]; 
+        allowances[_sender][_receiver] = currentallowances - _amount ;
+        return transfertemplate(_sender,_receiver,_amount); 
+    }
+
+    function transfertemplate(address from, address to, uint256 amount) private returns (bool) {
         uint256 inwallet = balances[from] ;
         require(inwallet >= amount , "Insufficient Funds");
-        require(msg.sender == from, "Access Denied");
         balances[from] = inwallet - amount ;
         balances[to] += amount ;
         return true;
