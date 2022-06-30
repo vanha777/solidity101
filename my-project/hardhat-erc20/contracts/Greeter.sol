cf87e6421e8f7c4b662c25e7e49df54570c5b4bc
vanha777 <vanha777@icloud.com>
2022-06-30 21:13:00 +1000

    Update Greeter.sol

--- my-project/hardhat-erc20/contracts/Greeter.sol
+++ my-project/hardhat-erc20/contracts/Greeter.sol
@@ -4,26 +4,19 @@ contract ERC20 {
     string public name ;
     string public symbol ;
     uint256 public supply;
+    uint256 public limit_supply;
     
     mapping(address=>uint256) public balances ;
     mapping(address=>mapping(address=>uint256)) public allowances;
 
-    constructor(string memory _name, string memory _symbol , uint256 _supply) {
+    constructor(string memory _name, string memory _symbol , uint256 _limit_supply) {
         _name = name ;
         _symbol = symbol ;
-        _supply = supply ;
+        _limit_supply = limit_supply ;
     }
 
     function decimals() external pure returns (uint8) {
         return 18;
-    }
-
-    function mint() public payable returns (bool) {
-        require(msg.sender != address(0) , "Recipient Address Invalid");
-        require(supply > 0 , "No More Token Can Be Minted");
-        balances[msg.sender] += msg.value ;
-        supply -= msg.value ;
-        return true ;
     }
 
     function transfer(address _to, uint256 value) external returns(bool) {
@@ -36,7 +29,7 @@ contract ERC20 {
         return true;
     }
 
-    function transferfrom(address _sender , address _receiver , uint256 _amount) external returns (bool) {
+    function transferfrom(address _sender , address _receiver , uint256 _amount) public returns (bool) {
         require(allowances[_sender][msg.sender] >= _amount , "Allowances Limit Reached") ;
         uint256 currentallowances = allowances[_sender][_receiver]; 
         allowances[_sender][_receiver] = currentallowances - _amount ;
@@ -49,5 +42,26 @@ contract ERC20 {
         balances[from] = inwallet - amount ;
         balances[to] += amount ;
         return true;
+    }
+
+
+  function deposit() public payable returns (bool) {
+        require(msg.sender != address(0) , "Recipient Address Invalid");
+        require(supply > 0 , "No More Token Can Be Minted");
+        uint256 x_value = msg.value ;
+        balances[msg.sender] += x_value ;
+        supply -= x_value ;
+        return true ;
+    }
+
+
+
+    function redeem(uint256 _value) external payable {
+        require(balances[msg.sender] >0 , "You Don't Have Any Tokens");
+        require(balances[msg.sender] >= _value , "Not Enough Tokens In Wallet");
+        balances[msg.sender] -= _value ;
+        (bool success, ) = msg.sender.call{value:_value * 1 wei}("");
+        require(success, "Transfer failed.");
+
     }
 }
